791ae4f932c0a0b07840de284b7d650b9c83c0c4
kevin <kmunene858@gmail.com>
2022-06-12 14:30:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+
+
+
+function superbowlWin(record){
+    for (let items of record){
+        if(items.result==='W'){
+            return(items.year)
+        }
+}
+}
+
+let findItem = record.find(superbowlWin);
+
+
+//   for (items of record){
+//     for(keys in items){
+//     if (keys.result === "W"){
+//       return keys['year'];
+//     }
+//   else{
+//     return undefined;
+//   }
+//   }
+//   }
+// }
+
+
+// console.log (findItem);
